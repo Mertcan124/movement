ef779457fa54edbeddeed57ec7392bccf0d6ddb3
Mertcan124 <kocamazmertcan3@gmail.com>
2022-05-15 23:16:11 +0200

    First Commit

---
+++ Assets/Scripts/Weapon/GunController.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GunController : MonoBehaviour
+{
+    [Header("Gun Settings")]
+    public float fireRate = 0.1f;
+    public int clipSize = 30;
+    public int reservedAmmoCapacity = 270;
+
+    //variables that change while code
+
+    bool _canShoot;
+    int _currentAmmoInClip;
+    int _ammoInReserve;
+
+
+    //Muzzle Flash
+    public Image muzzleFlashImage;
+    public Sprite[] flashes;
+
+    //Aiming
+    public Vector3 normalLocalPosition;
+    public Vector3 aimingLocalPosition;
+    public float aimSmoothing = 10;
+
+    //Weapon Recoil
+    public bool randomizeRecoil;
+    public Vector2 randomRecoilConstraints;
+//you only need to assign this if randomized recoil is off
+    public Vector2 recoilPattern;
+
+
+
+    private void Start()
+    {
+        _currentAmmoInClip = clipSize;
+        _ammoInReserve = reservedAmmoCapacity;
+        _canShoot = true;
+    }
+
+    private void Update()
+    {
+
+        DetermineAim();
+
+        if (Input.GetMouseButton(0) && _canShoot && _currentAmmoInClip > 0)
+        {
+            _canShoot = false;
+            _currentAmmoInClip--;
+            StartCoroutine(ShootGun());
+        }
+        else if(Input.GetKeyDown(KeyCode.R) && _currentAmmoInClip < clipSize && _ammoInReserve > 0)
+        {
+            int amountNeeded = clipSize - _currentAmmoInClip;
+            if(amountNeeded >= _ammoInReserve)
+            {
+                _currentAmmoInClip += _ammoInReserve;
+                _ammoInReserve -= amountNeeded;
+            }
+            else
+            {
+                _currentAmmoInClip = clipSize;
+                _ammoInReserve -= amountNeeded;
+            }
+        }
+    }
+
+    void DetermineAim()
+    {
+        Vector3 target = normalLocalPosition;
+        if(Input.GetMouseButton(1)) target = aimingLocalPosition;
+
+        Vector3 desiredPosition = Vector3.Lerp(transform.localPosition, target, Time.deltaTime * aimSmoothing);
+
+        transform.localPosition = desiredPosition;
+    }
+
+    void DeterminRecoil()
+    {
+        transform.localPosition -= Vector3.forward * 0.1f;
+
+     
+    }
+
+    IEnumerator ShootGun()
+    {
+
+        RayCastForEnemy();
+
+        DeterminRecoil();
+
+        StartCoroutine(MuzzleFlash());
+        yield return new WaitForSeconds(fireRate);
+        _canShoot = true;
+    }
+
+    IEnumerator MuzzleFlash()
+    {
+        muzzleFlashImage.sprite = flashes[Random.Range(0, flashes.Length)];
+        muzzleFlashImage.color = Color.white;
+        yield return new WaitForSeconds(0.05f);
+        muzzleFlashImage.sprite = null;
+        muzzleFlashImage.color = new Color(0, 0, 0, 0);
+    }
+
+    void RayCastForEnemy()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.parent.position, transform.parent.forward, out hit, 1 << LayerMask.NameToLayer("Enemy")))
+        {
+            try
+            {
+                Debug.Log("Hit an enemy");
+                Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.None;
+                rb.AddForce(transform.parent.transform.forward * 500);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+}
